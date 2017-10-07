7919c307dc5c60bb70e7ddbc981bf9fdc7949423
Will Mendes <willmendesneto@gmail.com>
2017-10-07 13:39:26 +1100

    refactor: adding flow types support

--- src/components/FeatureToggle/index.js
+++ src/components/FeatureToggle/index.js
@@ -1,17 +1,18 @@
+// @flow
 import React from 'react';
-import { string, node, bool } from 'prop-types';
 import { isOn } from 'feature-toggle-service';
 
-const FeatureToggle = (props) => {
+// Type aliases
+type FeatureToggleProps = {
+  featureName: string,
+  children: any,
+  showWhenDisabled: boolean,
+};
+
+const FeatureToggle = (props: FeatureToggleProps) => {
   const toggleState = isOn(props.featureName);
   const showContent = toggleState === !props.showWhenDisabled;
   return (showContent ? props.children : null);
-};
-
-FeatureToggle.propTypes = {
-  featureName: string.isRequired,
-  children: node.isRequired,
-  showWhenDisabled: bool,
 };
 
 export default FeatureToggle;
--- src/components/FeatureToggleProvider/index.js
+++ src/components/FeatureToggleProvider/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { objectOf, node, bool } from 'prop-types';
 import { setConfigurationObject } from 'feature-toggle-service';
 
-const FeatureToggleProvider = (props) => {
+// Type aliases
+type FeatureToggleProviderProps = {
+  featureToggleService: {| [string]: number |} ,
+  children: any,
+};
+
+
+const FeatureToggleProvider = (props: FeatureToggleProviderProps) => {
   setConfigurationObject(
     props.featureToggleService
   );
   return props.children;
-};
-
-FeatureToggleProvider.propTypes = {
-  featureToggleService: objectOf(bool),
-  children: node.isRequired,
 };
 
 export default FeatureToggleProvider;
